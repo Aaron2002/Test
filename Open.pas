6ec7936b7f9b1b6634c4dd92cfff806878193e50
Aaron(NSC) <ent.arn@outlook.com>
2014-09-12 13:21:16 +0900

    引数を型と同じにした。

--- Open.pas
+++ Open.pas
@@ -3,7 +3,7 @@ type TDynamicInteger = array of Integer;   // 動的配列
 procedure Test1(aDynamic: array of Integer);  // オープン配列
 procedure Test2(aDynamic: TDynamic);  // 動的配列
 
-function GetDynamicArray(const aDynamicArray: array of Integer): TDynamicInteger;
+function GetDynamicArray(const aDynamicArray: TDynamicInteger): TDynamicInteger;
 var a: Integer;
     d: TDynamicInteger;
 begin
