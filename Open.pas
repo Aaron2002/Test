d3063853b626309cd498746a370ccd1beca17921
Aaron(NSC) <ent.arn@outlook.com>
2014-09-12 13:22:57 +0900

    サンプルをユニットに設定。

--- Open.pas
+++ Open.pas
@@ -1,7 +1,13 @@
+unit Test;
+
+interface
+
 type TDynamic = array of Integer;   // 動的配列
 
 procedure Test1(aDynamic: array of Integer);  // オープン配列
 procedure Test2(aDynamic: TDynamic);  // 動的配列
+
+implementation
 
 function GetDynamicArray(const aDynamicArray: array of Integer): TDynamicInteger;
 var a: Integer;
@@ -11,3 +17,13 @@ begin
   for a := Low(aDynamicArray) to High(aDynamicArray) do d[a] := aDynamicArray[a];
   GetDynamicArray := d;
 end;
+
+procedure Test1(aDynamic: array of Integer);
+begin
+end;
+
+procedure Test2(aDynamic: TDynamic);
+begin
+end;
+
+end.
