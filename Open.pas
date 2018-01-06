7d6a977fdc1bb444c615d866dc48c8c03ffb0060
Aaron2002 <Aaron2002@users.noreply.github.com>
2018-01-06 11:19:29 +0900

    フォーマット修正
    フォーマットに修正を加える。

--- Open.pas
+++ Open.pas
@@ -2,13 +2,15 @@ unit Test;
 
 interface
 
-type TDynamicInteger = array of Integer;   // 動的に変更
+type
+  TDynamicInteger = array of Integer;   // 動的に変更
 
 implementation
 
 function GetDynamicArray(const aDynamicArray: array of Integer): TDynamicInteger;
-var a: Integer;
-    d: TDynamicInteger;
+var
+  a: Integer;
+  d: TDynamicInteger;
 begin
   SetLength(d, High(aDynamicArray) + 1);
   for a := 0 to High(aDynamicArray) do d[a] := aDynamicArray[a];
@@ -16,10 +18,6 @@ begin
 end;
 
 procedure Test1(aDynamic: array of Integer);
-begin
-end;
-
-procedure Test2(aDynamic: TDynamic);
 begin
 end;
 
